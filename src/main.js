b8e41253287e710e4655e4fb35eba6c10c58d918
CarlosSanches07 <carlos-caps@hotmail.com>
2019-07-14 23:53:04 -0300

    immplemented socket

--- src/main.js
+++ src/main.js
@@ -1,16 +1,30 @@
-const express = require("express");
-const body = require("body-parser")
+// const express = require("express");
+// const body = require("body-parser")
+// const http = require('http');
 const socket = require("socket.io");
 
-const app = express();
-app.use(body.json());
+// const app = express();
+// app.use(body.json());
+const io = socket(3000);
+
+
+/* app.get('/', (req, res) => {
+    console.log('e ai ');
+})
 
 app.post('/connect', (req, res) => {
-    console.log(req.body.data);
     res.status(200).send('ok');
 });
 
 app.listen('3000', () => {
     console.log('listen to 3000...');
-})
+}); */
+
+io
+    .on('connection', (socket) => {
+        socket.emit('connected');
+        socket.on('message',(data) => {    
+            socket.broadcast.emit('received',data);
+        })
+    })
 
